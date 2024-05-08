d33a956d1b25570c6888ec154982bec883922490
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-05-08 03:42:21 -0700

    vuefiles

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+ <h1>hayy there</h1>
+ <h3>how fast are u </h3>
+
+ <button @click="start" :disabled="isplaying">play</button>
+ <Block v-if="isplaying" :delay="delay" @end="endgame"/>
+ <p v-if="showresult">reaction time :{{ score}}</p>
+</template>
+
+
+<script >
+import Block from './components/Block.vue'
+export default {
+  components: {Block},
+  data(){
+    return{
+      isplaying: false,
+      delay:null,
+      score:null,
+      showresult:false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000+Math.random()*5000
+         this.isplaying = true
+    },
+    endgame(reactiofortime){
+        this.score = reactiofortime
+        this.isplaying = false
+        this.showresult = true
+    }
+  }
+}
+
+</script>
+<style >
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+.button{
+  background-color: aquamarine;
+}
+</style>
