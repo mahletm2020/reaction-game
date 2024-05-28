56626037648a92c2a238bade7c0e25199239a047
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-05-28 04:44:45 -0700

    blabla

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
   <p>reaction time {{ score }}ms</p>
+  <p>{{ rank }}</p>
 </template>
 
 <script>
 export default {
   props:['score'],
+  data(){
+    return{
+      rank:null
+    }
+  },
+  mounted(){
+    if (this.score<=700){
+      this.rank = 'daimn '
+    }else if(this.rank<=1000){
+      this.rank = 'not bad'
+    }else if(this.rank>2000){
+      this.rank = 'slow af get checked your daim finger'
+    }
+  }
 }
 
 </script>
